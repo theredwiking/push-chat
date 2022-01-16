cd655dd6191a3669f86bf7f57d1d145cba95c06b
theredwiking <hjulmandn@gmail.com>
2022-01-16 14:01:40 +0100

    Can receive pushs

--- server.js
+++ server.js
@@ -5,8 +5,36 @@ dotenv.config();
 const pusher = new Pushbullet(process.env.TOKEN)
 
 const stream = pusher.stream();
-stream.connect();
 
 stream.on('connect', () => {
     console.log('Connected');
-})
+});
+
+stream.on('close', () => {
+    console.log('closed');
+});
+
+stream.on('error', (error) => {
+    console.log(error);
+});
+
+stream.on('tickle', () => {
+    pusher.history({limit: 5, modified_after: (Math.floor(new Date().getTime() / 1000) -1)}, (error, response) => {
+        if (error) console.log(error);
+        switch(response.pushes[0].type) {
+            case 'link':
+                console.log(response.pushes[0].url);
+                break;
+            case 'note':
+                console.log(response.pushes[0].body);
+                break;
+            case 'file':
+                console.log(response.pushes[0].file_url);
+                break;
+            default:
+                console.log(response.pushes);
+        }
+    });
+});
+
+stream.connect();
