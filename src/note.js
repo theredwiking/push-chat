bc40434a201148ba07e102b3d84612f24a8c53a9
Niclas H <nihj@redship.redship.lan>
2022-01-19 14:18:50 +0100

    Working on web status and need to add snmp

--- src/note.js
+++ src/note.js
@@ -1,3 +1,20 @@
+const https = require('https');
+const { exit } = require('process');
+
+let getRequest = (options, callback) => {
+    https.request(options, (response) => {
+        let str = '';
+
+        response.on('data', (chunk) => {
+            str += chunk;
+        });
+
+        response.on('end', () => {
+            callback(str);
+        });   
+    }).end();
+}
+
 exports.Cases = (sender, msg, pusher) => {
     switch(msg) {
         case 'System status':
@@ -5,7 +22,25 @@ exports.Cases = (sender, msg, pusher) => {
                 if (err) console.log(err);
             });
             break;
+            
+        case 'Web status':
+            let sites = {};
+            
+            getRequest({host: 'jellyfin.theredwiking.com', path: '/health', port: 443}, (str) => {
+                if(str !== 'Healthy') {
+                    console.log(str);
+                }
+                console.log(str);
+            });
+
+            console.log(sites);
+
+            pusher.note(sender, "Websites", `${JSON.stringify(sites)}`, (err, resp) => {
+                if (err) console.log(err);
+            });
+            break;
+
         default:
-            console.log(msg);  
+            console.log(msg);
     }
 }
